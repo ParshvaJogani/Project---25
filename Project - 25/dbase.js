66719647e7edbeb38af6feb7e1aec49ed23054f6
ParshvaJogani <69082393+ParshvaJogani@users.noreply.github.com>
2020-09-04 21:44:07 +0530

    Add files via upload

---
+++ Project - 25/dbase.js
@@ -0,0 +1,13 @@
+class dbase {
+    constructor() {
+        this.body = Bodies.rectangle(650,450,200,20);
+        this.image = loadImage("dustbingreen.png")
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        Matter.Body.setStatic(this.body, true);		
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y, 200, 200);
+    }
+}
